c94dbee7905454df2bc191c0ccc3ac6fbc43751b
Mina-projects <57490864+Mina-projects@users.noreply.github.com>
2019-11-20 19:27:23 +0100

    editing the rotation
    having a cooler rot

--- Fight for the planet/Fight for the planet/Assets/Scripts/Player.cs
+++ Fight for the planet/Fight for the planet/Assets/Scripts/Player.cs
@@ -13,6 +13,17 @@ public class Player : MonoBehaviour
     float xRange = 0.06f;
     [SerializeField]
     float yRange = 0.6f;
+    [SerializeField]
+    float positionPitchFactor = -30f;
+    [SerializeField]
+    float controlPitchFactor = -86f;
+    [SerializeField]
+    float positionYawFactor = -10f;
+    [SerializeField]
+    float controlRollFactor = -40f;
+
+
+    float horizonatlThrow, verticalThrow;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,31 +34,40 @@ public class Player : MonoBehaviour
     void Update()
     {
         MovementControl();
-        RotationControl();
-        
+        RotationControl();    
     }  
 
 
     public void MovementControl()
     {
-        float horizonatlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // access to the horizontal and vertical axis of this game object
-        float verticalThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        horizonatlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // access to the horizontal and vertical axis of this game object
+        verticalThrow = CrossPlatformInputManager.GetAxis("Vertical");
 
         float xOffset = horizonatlThrow * SpeedFactor * Time.deltaTime; // controling speed with an offset
         float yOffset = verticalThrow * SpeedFactor * Time.deltaTime;
 
-        float rawNewxPos = transform.localPosition.x + xOffset; // add the offset to the position
+        float rawNewxPos = transform.localPosition.x + xOffset; // add the offset to the position, by this you can move the object. 
         float rawNewyPos = transform.localPosition.y + yOffset;
 
         float clampXPos = Mathf.Clamp(rawNewxPos, -xRange, xRange); // limiting the movements
         float clampYPos = Mathf.Clamp(rawNewyPos, -yRange, yRange);
-
 
         transform.localPosition = new Vector3(clampXPos, clampYPos, transform.localPosition.z);
     }
 
     public void RotationControl()
     {
-        transform.localRotation = Quaternion.Euler(0, 90, 0);
+        //pitch
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = verticalThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        //yaw
+        float yaw = transform.localPosition.x * positionYawFactor;
+
+        //roll
+        float roll = horizonatlThrow * controlRollFactor; 
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
